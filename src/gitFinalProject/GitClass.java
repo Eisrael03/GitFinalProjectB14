a52ae0c1c8be997b229a69eb732df44473755cb4
Eisrael03 <eisrael2203@gmail.com>
2022-09-28 20:49:49 -0400

    2nd cdommit

--- src/gitFinalProject/GitClass.java
+++ src/gitFinalProject/GitClass.java
@@ -3,7 +3,7 @@ package gitFinalProject;
 public class GitClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("This is the code for the Mercedes");
 
 	}
 
